1ac3f235d52f218ae47c1d843273e0f98c37e125
Tom Brown <tomgith4@thecap.org>
2018-12-23 02:28:57 -0800

    merging in master

--- examples/geo_alchemy/config.py
+++ examples/geo_alchemy/config.py
@@ -6,8 +6,8 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://flask_admin_geo:flask_admin_geo
 SQLALCHEMY_ECHO = True
 
 # credentials for loading map tiles from mapbox
-MAPBOX_MAP_ID = '...'
-MAPBOX_ACCESS_TOKEN = '...'
+MAPBOX_MAP_ID = 'mapbox.streets'
+MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidG9tZ29icmF2byIsImEiOiJjajZwZzVyZnYwdGZlMnFvMTZyaXR3bmU3In0.fM7v2OUbs3hsBgwgioVIaA'
 
 # when the creating new shapes, use this default map center
 DEFAULT_CENTER_LAT = -33.918861
