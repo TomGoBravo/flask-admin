81f8965b6adf6e24a25d1bf9c562f2e12f4c9b30
Tom Brown <tombrown@lyft.com>
2019-11-24 01:57:16 +0000

    disable marker. it misleads about the scale and threw an exception.

--- flask_admin/static/admin/js/form.js
+++ flask_admin/static/admin/js/form.js
@@ -177,7 +177,7 @@
 		jsonpParam: 'json_callback',
 		propertyName: 'display_name',
 		propertyLoc: ['lat','lon'],
-		marker: L.circleMarker([0,0],{radius:30}),
+		marker: false,
 		autoCollapse: true,
 		autoType: false,
 		minLength: 2
